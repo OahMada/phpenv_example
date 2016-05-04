2e66e8676a665166dc41892ae32d064605f9d069
Zonggang Hao <haozg44@gmail.com>
2016-05-04 20:37:41 +0800

    First commit

---
+++ public/index.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once '../vendor/autoload.php';
+
+echo "Hello world";
+
+$database = new medoo([ 
+    'database_type' => 'sqlite', 
+    'database_file' => '../storage/database.db' 
+]);
+
+//dump($database);
+?>
